734d3205b9a24a60c2d5c18377a02e9536113c88
Koz-Koz <39735171+Koz-Koz@users.noreply.github.com>
2018-06-29 15:23:22 +0200

    Delete BMFWS.js

--- BMFWS.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");}, 10000);
-
-    
-function color2() {
-
-    document.getElementById("couleur").style.backgroundColor = "#FF00FF";
-}
