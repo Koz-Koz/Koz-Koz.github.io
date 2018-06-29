d3ed62529c568579977b506a254ffd84e483fa4f
Koz-Koz <39735171+Koz-Koz@users.noreply.github.com>
2018-06-29 14:44:09 +0200

    Add files via upload

---
+++ BMFWS.js
@@ -0,0 +1,7 @@
+setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");}, 10000);
+
+    
+function color2() {
+
+    document.getElementById("couleur").style.backgroundColor = "#FF00FF";
+}
